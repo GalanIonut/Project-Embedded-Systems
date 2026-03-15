876335b4f5e2cb351f5a9a37f9ef6c40188c608b
Ionut Galan <93884682+GalanIonut@users.noreply.github.com>
2026-03-15 23:41:56 +0200

    Update `.gitignore` to exclude development files.

--- src/main.c
+++ src/main.c
@@ -4,18 +4,25 @@
 #include "drivers/timer/timer0.h"
 #include <avr/io.h>
 #include "drivers/adc/adc.h"
-#include "stdio.h"
+#include <stdio.h>
+#include <string.h>
 #include "utils/delay.h"
-int main()
+
+int main(void)
 {
     Timer0_Init();
     USART_Init_Default();
     ADC_Init();
-    uint16_t adc_read;
+    
+    uint16_t adc_read = 0;
     char message[30] = { 0 };
+    
     while (1) {
+        adc_read = ADC_Read(0);
         sprintf(message, "%d \n", adc_read);
-        USART_Transmit(&message, sizeof(message));
+        USART_Transmit(message, strlen(message));
         Delay(300);
     }
+    
+    return 0;
 }
